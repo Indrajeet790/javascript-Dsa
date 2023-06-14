0c77419dbb130af1cac31db3fe986546c792ddd4
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-14 12:31:33 +0530

    insert element into array and also using splice function

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 // question 1
 // Traverse each element in array
-let data = [2, 4, 6, 8, 10, 11, 45, 34, 27];
-let x = 5;
-for (let i = 0; i < data.length; i++) {
-  console.log(data[i]);
+// let data = [2, 4, 6, 8, 10, 11, 45, 34, 27];
+
+// for (let i = 0; i < data.length; i++) {
+//   console.log(data[i]);
+// }
+
+// // question2 access array element
+// let arr = [2, 4, 6, 8, 10, 11, 45, 34, 27];
+// let x = 5;
+// let y = 8;
+// console.log(arr[x]);
+// console.log(arr[y]);
+
+// question 3
+// insert element into ARRAY
+let data = [10, 20, 30, 40, 50, 60];
+let newEl = 35;
+let position = 3;
+
+for (let i = data.length - 1; i > 0; i--) {
+  // console.log(data[i]);
+  // check position
+  if (i >= position) {
+    data[i + 1] = data[i];
+    if (i == position) {
+      data[i] = newEl;
+    }
+  }
 }
+console.log(data);
+
+// using splice function
+let item = [10, 20, 30, 40, 50, 60];
+item.splice(4, 0, 45);
+console.log(item);
