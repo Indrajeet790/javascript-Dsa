3a44aa1c384842b78e36188742f886c807caeb90
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-17 23:19:53 +0530

    sorting array with using bubble short

--- script.js
+++ script.js
@@ -82,3 +82,20 @@ for (let i = 0; i < arr2.length; i++) {
 }
 console.log(arrFinal);
 */
+
+// #######################//
+// array sorting
+// nested loop
+// using bubble short
+
+let data = [40, 20, 30, 12, 25];
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    if (data[j] > data[j + 1]) {
+      let temp = data[j];
+      data[j] = data[j + 1];
+      data[j + 1] = temp;
+    }
+  }
+}
+console.log(data);
